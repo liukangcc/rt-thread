e06288d3cdc5bc3959f2e1485bc0c357f0332e48
liukangcc <liukangcc@outlook.com>
2021-11-10 14:41:24 +0800

    lvgl

--- bsp/stm32/stm32f469-st-disco/board/ports/lvgl/lv_conf.h
+++ bsp/stm32/stm32f469-st-disco/board/ports/lvgl/lv_conf.h
@@ -13,5 +13,12 @@
 
 #define LV_USE_PERF_MONITOR 1
 #define LV_COLOR_DEPTH  32
+#define LV_USE_GPU              1 
+#define LV_USE_GPU_STM32_DMA2D  1
+#if LV_USE_GPU_STM32_DMA2D
+/*Must be defined to include path of CMSIS header of target processor
+e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
+#define LV_GPU_DMA2D_CMSIS_INCLUDE "stm32f429xx.h"
+#endif
 
 #endif
